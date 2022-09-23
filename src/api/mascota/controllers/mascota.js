baed1341baebf910bba62a827dc56ce7fe311350
Jorge Startop <jorge@thestartop.com>
2022-09-22 20:10:19 -0500

    se agrego modelo de mascota

--- src/api/mascota/controllers/mascota.js
+++ src/api/mascota/controllers/mascota.js
@@ -1,9 +1,9 @@
 'use strict';
 
 /**
- * test controller
+ * mascota controller
  */
 
 const { createCoreController } = require('@strapi/strapi').factories;
 
-module.exports = createCoreController('api::test.test');
+module.exports = createCoreController('api::mascota.mascota');
--- src/api/mascota/routes/mascota.js
+++ src/api/mascota/routes/mascota.js
@@ -1,9 +1,9 @@
 'use strict';
 
 /**
- * test router
+ * mascota router
  */
 
 const { createCoreRouter } = require('@strapi/strapi').factories;
 
-module.exports = createCoreRouter('api::test.test');
+module.exports = createCoreRouter('api::mascota.mascota');
